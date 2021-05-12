452755ab84ea5bf66adae4d0612fb2bfa07b9f80
Kevin Song <kevinmnm2010@gmail.com>
2021-05-11 21:00:48 -0400

    Default

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+   <v-sheet class="d-flex justify-center align-center" width="100%" height="100%" dark>
+      Hello
+   </v-sheet>
+</template>
+
+<script>
+export default {
+   name: "MyComponent",
+   data:()=>({
+
+   })
+};
+</script>
+
+<style>
+</style>
